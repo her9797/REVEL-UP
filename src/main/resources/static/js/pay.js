0d6d4697fb70a64fe55a511cb7715bf1bd28a3d7
her97 <simkoon0120@gmail.com>
2024-03-06 17:08:53 +0900

    0306 16시 커밋

---
+++ src/main/resources/static/js/pay.js
@@ -0,0 +1,21 @@
+function postApi() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            var addr = ''; // 주소 변수
+
+            // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('userPostcode').value = data.zonecode;
+            document.getElementById("userAdd").value = addr;
+
+            // 상세주소 필드로 커서 이동
+            document.getElementById("userDetailAddress").focus();
+        }
+    }).open();
+}
