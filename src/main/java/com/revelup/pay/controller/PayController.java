32dce3ff283e7328a09b5c3007ba9bbe180ba655
mins199600 <mins19960091@gmail.com>
2024-03-07 15:45:41 +0900

    카카오 페이 api 연결중입니다

--- src/main/java/com/revelup/pay/controller/PayController.java
+++ src/main/java/com/revelup/pay/controller/PayController.java
@@ -1,15 +1,38 @@
 package com.revelup.pay.controller;
 
+import com.revelup.pay.model.dto.PayDTO;
+import com.revelup.pay.model.service.PayService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/content/pay")
 public class PayController {
 
+    @RestController
+    @RequestMapping("/pay")
+    @RequiredArgsConstructor
+    public class KakaoPayController {
+
+        private final PayService.KakaoPayService kakaoPayService;
+
+        /**
+         * 결제요청
+         * @return
+         */
+        @PostMapping("/ready")
+        public PayDTO.KakaoReadyResponse readyToKakaoPay() {
+
+            return kakaoPayService.kakaoPayReady();
+        }
+
+
     @GetMapping("/pay")
     public String payPage() {
         return "content/pay/pay";
     }
-}
+
