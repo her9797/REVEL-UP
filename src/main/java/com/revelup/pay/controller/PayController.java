4a29268cecacafb149a75d79cef68f3648584457
mins199600 <mins19960091@gmail.com>
2024-03-08 15:52:47 +0900

    pay 내용추가

--- src/main/java/com/revelup/pay/controller/PayController.java
+++ src/main/java/com/revelup/pay/controller/PayController.java
@@ -1,38 +1,20 @@
 package com.revelup.pay.controller;
 
-import com.revelup.pay.model.dto.PayDTO;
-import com.revelup.pay.model.service.PayService;
-import lombok.RequiredArgsConstructor;
+import com.revelup.pay.model.dao.PayDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/content/pay")
 public class PayController {
-
-    @RestController
-    @RequestMapping("/pay")
-    @RequiredArgsConstructor
-    public class KakaoPayController {
-
-        private final PayService.KakaoPayService kakaoPayService;
-
-        /**
-         * 결제요청
-         * @return
-         */
-        @PostMapping("/ready")
-        public PayDTO.KakaoReadyResponse readyToKakaoPay() {
-
-            return kakaoPayService.kakaoPayReady();
-        }
-
+   // @Autowired
+   // PayDAO payDAO;
+   // @RequestMapping("/pay_complect")
 
     @GetMapping("/pay")
     public String payPage() {
         return "content/pay/pay";
     }
-
+}
