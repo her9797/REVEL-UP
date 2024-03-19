f734e1bdf9271fb0aac26afc44fb1a7e2c70afaa
jaylee <jayginwoolee98@gmail.com>
2024-03-20 08:52:27 +0900

    Jay_240320-01 : 메인페이지 상세보기 연결 완료

--- src/main/java/com/revelup/funding/model/service/FundingServiceImpl.java
+++ src/main/java/com/revelup/funding/model/service/FundingServiceImpl.java
@@ -83,14 +83,14 @@ public class FundingServiceImpl implements FundingService {
 
             // 파일 저장용 폴더에 파일 저장 처리
 //            String fndFileLoc = "C:/Users/thunder/Desktop/revelup/" + fndSaveFile; // 이진우 Mac용 저장경로
-//            String fndFileLoc = "/Users/jaylee/Documents/SemiFinal/fndFileLoc/" + fndSaveFile; // 이진우 Mac용 저장경로
-        String fndFileLoc = "C:/Users/hi/Desktop/revelupimg/" + fndSaveFile; // Window용 저장경로
+            String fndFileLoc = "/Users/jaylee/Documents/SemiFinal/fndFileLoc/" + fndSaveFile; // 이진우 Mac용 저장경로
+//        String fndFileLoc = "C:/Users/hi/Desktop/revelupimg/" + fndSaveFile; // Window용 저장경로
 //        String fndFileLoc = "C:\\Users\\simko\\Desktop\\file/" + fndSaveFile; // Window용 저장경로
             file.transferTo(new File(fndFileLoc));
 
-//            String saveFile = "/Users/jaylee/Documents/SemiFinal/fndFileLoc/" + fndSaveFile;
+            String saveFile = "/Users/jaylee/Documents/SemiFinal/fndFileLoc/" + fndSaveFile;
 //            String saveFile = "C:/Users/thunder/Desktop/revelup/" + fndSaveFile;
-            String saveFile = "C:\\Users\\simko\\Desktop\\file/" + fndSaveFile;
+//            String saveFile = "C:\\Users\\simko\\Desktop\\file/" + fndSaveFile;
             sessionData.setSessionAttribute("saveFile", saveFile);
 
             // FundingFileDTO 세팅 및 DB에 삽입
@@ -111,8 +111,8 @@ public class FundingServiceImpl implements FundingService {
             String siSaveFile = System.currentTimeMillis() + "-" + siOrgFile;
 
             // 파일 저장용 폴더에 파일 저장 처리
-//            String siFileLoc = "/Users/jaylee/Documents/SemiFinal/fndFileLoc/" + siSaveFile; // 이진우 Mac용 저장경로
-        String siFileLoc = "C:/Users/hi/Desktop/revelupimg/" + siSaveFile; // 현지 Window용 저장경로
+            String siFileLoc = "/Users/jaylee/Documents/SemiFinal/fndFileLoc/" + siSaveFile; // 이진우 Mac용 저장경로
+//        String siFileLoc = "C:/Users/hi/Desktop/revelupimg/" + siSaveFile; // 현지 Window용 저장경로
 //        String siFileLoc = "C:/Users/thunder/Desktop/revelup/" + siSaveFile; // 현지 Window용 저장경로
 //        String siFileLoc = "C:/Users/simko/Desktop/file/" + siSaveFile; // 현지 Window용 저장경로
 
